3e3f5ebb0bf9949a0b01e17a5dee5898f1da41d8
goldipl <mgodlewski92@gmail.com>
2022-05-25 22:45:26 +0200

    add FlatList 3-cols

--- components/Photos.js
+++ components/Photos.js
@@ -1,5 +1,5 @@
 import { useQuery } from "react-query";
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, FlatList } from 'react-native';
 
 async function fetchPhotos() {
     const response = await fetch('https://jsonplaceholder.typicode.com/photos');
@@ -9,16 +9,25 @@ async function fetchPhotos() {
 export function Photos() {
     const {data, isLoading, isError} = useQuery('photos', fetchPhotos, {placeholderData: []});
 
-    return <View style={styles.container}>
+    return (
+    
+    <FlatList
+      data={[1, 2, 3, 4]}
+      numColumns={3}
+      renderItem={() => (
+        <View style={styles.container}>
         <Text>Photos</Text>
         {isLoading && <Text>Loading...</Text>}
         {isError && <Text>Error</Text>}
         {data.map(photos => <Image 
             key={photos.id} 
-            style={{ width: 150, height: 150 }}
+            style={{ width: '33vw', height: 150 }}
             source={{ uri: photos.thumbnailUrl}}
         />)}
     </View>
+      )}
+    />
+    )
 }
 
 const styles = StyleSheet.create({
